428e08542284953ad78c0f58c5464115958a793e
eb19236 <elisheva19236@gmail.com>
2026-03-13 13:57:07 +0200

    Update Home.jsx

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <Link to="/ProjectsList">פרויקטים שלי</Link>
-      <br />
+      {/* לא ידענו אם צריך את זה אז השארנו את זה ככה */}
+      {/* <Link to="/ProjectsList">פרויקטים שלי</Link>
+      <br /> */}
       <Link to="/Login">Login-להרשמה ולהתחברות</Link>
     </>
   );
